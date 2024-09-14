7b0902010bbb5ec48a326c328b2b5f6e5f236d30
tejas122125 <tejasweekumarsingh@gmail.com>
2024-09-14 23:24:50 +0530

    tejas:

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -8,22 +8,25 @@ const Onboarding = () => {
     const swiperRef = useRef<Swiper>(null);
     const [index, setIndexValue] = useState(0);
     return (
-        <SafeAreaView className="flex bg-pink-400 justify-between items-center">
+        <SafeAreaView className="flex h-full w-full bg-pink-400 flex-col justify-center items-center">
             <TouchableOpacity onPress={() => {
                 router.replace("/(auth)/sign-in")
             }} className="bg-blue-300 w-full flex justify-end items-end p-5">
                 <Text className="text-black text-base font-JakartaBold">Skip</Text>
             </TouchableOpacity>
-            <Swiper ref={swiperRef} loop={false} dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]" />}
+           
+            <Swiper className="bg-green-300" ref={swiperRef} loop={false} dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]" />}
                 activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />}
                 onIndexChanged={(index) => { setIndexValue(index) }}>
                     {onboarding.map((item) =>
                     {
-                        
+                        return (
+                            <View>
+                                <Text>{item.title}</Text>
+                            </View>
+                        )
                     })}
-                <View>
-                    <Text>text </Text>
-                </View>
+       
 
             </Swiper>
             <Text>
